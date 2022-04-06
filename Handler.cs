24dd1139c464b3179b6562e313d4251410df6a25
Vilhaem <williamliu414@gmail.com>
2022-04-07 00:04:17 +0800

    Discarded WebAssembly Using Webapp

--- Handler.cs
+++
@@ -1,9 +0,0 @@
-//using Microsoft.AspNetCore.Mvc;
-
-namespace NTUT_Resturant_Finding_Assistant.Model;
-
-    public class Handler
-    {
-        public void getResturant(Resturant resturant){}
-        
-    }
